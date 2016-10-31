5bbc00ec43c92b0d671ac505890cde9f420937e1
Apple <mm1073856565>
2016-10-31 14:33:48 +0800

    啊

---
+++ Test/Test/MLObject.swift
@@ -0,0 +1,16 @@
+//
+//  MLObject.swift
+//  Test
+//
+//  Created by Apple on 16/10/31.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+import UIKit
+
+open class MLObject: NSObject {
+
+    open func printNumber(number: Int) {
+        print("\(number)")
+    }
+}
---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Apple on 16/10/31.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
